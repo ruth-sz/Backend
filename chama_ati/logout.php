c21a9701d613f5a5c4ca00bdcdd4dd93db76f895
Ruth S. Z. <ruthsouzazahn@gmail.com>
2025-08-22 20:08:01 -0300

    Delete chama_ati directory

--- chama_ati/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-session_destroy();
-header('Location: index.php'); /*obriga um redirecionamento pois e necessario fazer uma nova requisicao
-para remover as variaveis de sessao do lado do servidor que sao recuperadas pela instancia da aplicacao*/
-?>
--- chama_ati/validador_acesso.php
+++
@@ -1,7 +0,0 @@
-<?php
-//logica de validacao pra sessao - usuario nao autenticado é redirecionado pra pagina inicial pra fazer login
-  session_start();
-  if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM'){
-    header('Location: index.php?login=erro2_faca_login');
-  }
-?>
