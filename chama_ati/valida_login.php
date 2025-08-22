b117569f25eebc39e1bd1d3a3a9e03d5a2592ad0
Ruth S. Z. <ruthsouzazahn@gmail.com>
2025-08-22 20:05:41 -0300

    chama_ati

--- chama_ati/valida_login.php
+++ chama_ati/valida_login.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 session_start();
 
 //Variavel que verifica se a autenticacao foi feita
@@ -38,12 +37,3 @@ if($usuario_autenticado){
     header('Location: index.php?login=erro_login_invalido');
 }
 ?>
-=======
-    require "R:/XAMPP/chama_ati/valida_login.php" //sobe um nivel hierarquico do diretorio
-    
-    /* se algum usuario mal intencionado tiver acesso ao codigo fonte desse script, por estar no diretorio publico,
-    ele so vera a instrucao de require, pois o conteudo do arquivo estara protegido pelo firewall do SO e longe 
-    do acesso do servidor http, mas a aplicacao quando interpretada pelo php do lado do servidor pode localizar 
-    qualquer arquivo do SO */
-?>
->>>>>>> 31858b1 (htdocs)
